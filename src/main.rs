0e65306e1ea3dafc45ad031af3e1279c93370853
5HT <maxim@synrc.com>
2024-07-28 20:51:59 +0300

    code review take one

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-mod ast;
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod!(pub grammar);
+pub mod ast;
+fn print(s: &str) {
+    let res = grammar::ExprParser::new().parse(s).unwrap();
+    println!("Term: {:?}", res);
+    println!("Type: {:?}", res.type_check());
+    println!("");
+}
+
 fn main() {
-    println!("Hello, world!");
+   print("Σ (x:*), x");
 }
