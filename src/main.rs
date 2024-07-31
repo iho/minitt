dd24a874ef2026bb35d27d36eeffe65fe295971f
5HT <maxim@synrc.com>
2024-07-31 22:54:23 +0300

    check

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 use lalrpop_util::lalrpop_mod;
-use crate::check::check_main;
-mod check;
-pub mod eval;
-pub mod pretty;
 
 lalrpop_mod!(pub grammar);
 pub mod ast;
 fn print(s: &str) {
     let res = grammar::ExprParser::new().parse(s).unwrap();
     println!("Term: {:?}", res);
-    println!("Type: {:?}", check_main(res));
+    println!("Type: {:?}",crate::ast::check_main(&res));
     println!("");
 }
 
 fn main() {
    print("Π (n: *), Σ (x: *), n");
-   print("Σ (y x: *1), (x, x)");
+   print("Σ (y x: *), *");
 }
